4935ad446ffbc33e18927e6d761f99b2ec2b23da
Akshay <agurav732@gmail.com>
2022-09-04 15:49:25 +0530

    created employee table with API show and insert API

---
+++ app/Http/Controllers/EmployeeApiController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Employee;
+
+class EmployeeApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Employee::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+          $this->validate($request,[
+         'name'=> 'required',
+          'email'=> 'required|string|unique:employees,email',
+         'phone'=>'required|numeric|unique:employees,phone',
+         'address'=>'required|string',
+         'department_id'=>'required'
+
+      ]);
+       
+        return Employee::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('get_departments', 'DepartmentController@get_departments');
 Route::resource('departments',DepartmentApiController::class);
+Route::resource('employee',EmployeeApiController::class);
 Route::post('add_department', 'DepartmentController@add_department');
 // Route::get('delete_departments', 'DepartmentController@get_departments');
 // Route::get('delete_departments', 'DepartmentController@get_departments');
